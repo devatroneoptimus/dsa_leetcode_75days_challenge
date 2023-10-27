c3f1c0a41f4bd1299946b7ba150bdc850831d900
devdastl <deveshdashore@gmail.com>
2023-10-28 00:59:16 +0530

    UPDATE: logic for termination

--- day_1/alternate_merge.py
+++ day_1/alternate_merge.py
@@ -4,8 +4,6 @@ new_w = []
 
 leng = len(w1) + len(w2)
 
-w1 += ' '
-w2 += ' '
 
 len1 = True
 len2 = True
@@ -17,7 +15,7 @@ pw2 = 0
 for i in range(1, leng+3):
   if i % 2 == 0 and len2:
     print(w2[pw2])
-    len2 = w2[pw2] != ' '
+    len2 = len(w2) > pw1
     if len2:
       new_w.append(w2[pw2])
       pw2 += 1
@@ -25,7 +23,7 @@ for i in range(1, leng+3):
       break
 
   elif len1:
-    len1 = w1[pw1] != ' '
+    len1 = len(w1) > pw2
     if len1:
       print(w1[pw1])
       new_w.append(w1[pw1])
@@ -34,7 +32,7 @@ for i in range(1, leng+3):
       break
 
 if len(w1)>len(w2):
-  new_w.append(w1[pw1:-1])
+  new_w.append(w1[pw1:])
 else:
   new_w.append(w2[pw2:-1])
   print(len1, len2)
